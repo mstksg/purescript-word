fb0561854e200f458c44a1c508e24d92b5feae0f
Ubuntu <ubuntu@pmcloud.world>
2017-06-19 03:41:22 +0000

    Fix

--- src/Data/Integral/Integral.purs
+++ src/Data/Integral/Integral.purs
@@ -9,8 +9,6 @@ import Data.BigInt as B
 class Integral a where
     fromBigInt :: B.BigInt -> a
     toBigInt :: a -> B.BigInt
-
---foreign import data BigInt :: Type
     
 fromIntegral :: forall a b. Integral a => Integral b => a -> b
 fromIntegral = fromBigInt <<< toBigInt
