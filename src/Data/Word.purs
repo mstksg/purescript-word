b5f093e199f22061a031ccb45277b6f4c42bc6cd
Ubuntu <ubuntu@pmcloud.world>
2017-06-19 13:55:47 +0000

    Word8

--- src/Data/Word.purs
+++ src/Data/Word.purs
@@ -1,6 +1,7 @@
 module Data.Word
        ( Word
        , Word32
+       , Word8
        ) where
        
 import Prelude
@@ -11,7 +12,18 @@ import Data.Bits (class Bits)
 import Data.Integral (class Integral)
 import Data.UInt (UInt, fromInt, fromNumber, toInt) as U
 import Data.UInt.Bits as B
-       
+
+-- Inelegant brute force conversion
+showHex :: U.UInt -> String
+showHex b | b < (U.fromInt 10) = take 1 $ show b
+showHex b | b == (U.fromInt 10) = "A"
+showHex b | b == (U.fromInt 11) = "B"
+showHex b | b == (U.fromInt 12) = "C"
+showHex b | b == (U.fromInt 13) = "D"
+showHex b | b == (U.fromInt 14) = "E"
+showHex b | b == (U.fromInt 15) = "F"
+showHex b = "#" <> show b <> "#"
+
 -- | A generic Word32
 newtype Word32 = Word32 U.UInt
 type Word = Word32
@@ -26,17 +38,6 @@ instance showWord32 :: Show Word32 where
                                   <> showHex (B.and (B.shr a (U.fromInt 4)) (U.fromInt 0xF))  
                                   <> showHex (B.and a (U.fromInt 0xF))
                                   <> " (" <> show a <> ")"
-        where
-        -- Inelegant brute force conversion
-        showHex :: U.UInt -> String
-        showHex b | b < (U.fromInt 10) = take 1 $ show b
-        showHex b | b == (U.fromInt 10) = "A"
-        showHex b | b == (U.fromInt 11) = "B"
-        showHex b | b == (U.fromInt 12) = "C"
-        showHex b | b == (U.fromInt 13) = "D"
-        showHex b | b == (U.fromInt 14) = "E"
-        showHex b | b == (U.fromInt 15) = "F"
-        showHex b = "#" <> show b <> "#"
 
 instance eqWord32 :: Eq Word32 where
     eq (Word32 a) (Word32 b) = a == b
@@ -54,18 +55,6 @@ instance semiringWord32 :: Semiring Word32 where
     add (Word32 a) (Word32 b) = Word32 (a+b)
     mul (Word32 a) (Word32 b) = Word32 (a*b)
 
---fromInt :: Int -> Word32
---fromInt = Word32 <<< ((add (U.fromInt 0)) :: U.UInt -> U.UInt) <<< U.fromInt
-
---fromUInt :: U.UInt -> Word32
---fromUInt = Word32
-
---toInt :: Word32 -> Int
---toInt (Word32 a) = U.toInt a
-
---toUInt :: Word32 -> U.UInt
---toUInt (Word32 a) = a
-
 instance word32Integral :: Integral Word32 where
     fromBigInt bi = Word32 $ U.fromNumber <<< BI.toNumber $ bi
     toBigInt (Word32 a) = BI.fromInt <<< U.toInt $ a
@@ -78,3 +67,58 @@ instance word32Bits :: Bits Word32 where
     shl (Word32 a) s = Word32 $ B.shl a s
     shr (Word32 a) s  = Word32 $ B.shr a s
     zshr (Word32 a) s = Word32 $ B.zshr a s
+
+-- | A generic Word8
+newtype Word8 = Word8 U.UInt
+
+instance showWord8 :: Show Word8 where
+    show (Word8 a) = "Word8 0x" <> showHex (B.and (B.shr a (U.fromInt 4)) (U.fromInt 0xF))  
+                                <> showHex (B.and a (U.fromInt 0xF))
+                                <> " (" <> show a <> ")"
+
+instance eqWord8 :: Eq Word8 where
+    eq (Word8 a) (Word8 b) = (B.and a (U.fromInt 0xFF)) == (B.and b (U.fromInt 0xFF))
+
+instance ordWord8 :: Ord Word8 where
+    compare (Word8 a) (Word8 b) = compare (B.and a (U.fromInt 0xFF))  (B.and b (U.fromInt 0xFF))
+
+instance boundedWord8 :: Bounded Word8 where
+    bottom = Word8 $ U.fromInt 0
+    top = Word8 $ B.and (B.complement (U.fromInt 0)) (U.fromInt 0xFF)
+
+instance semiringWord8 :: Semiring Word8 where
+    zero = bottom
+    one = Word8 $ U.fromInt 1
+    add (Word8 a) (Word8 b) = Word8 $ B.and (a+b) (U.fromInt 0xFF)
+    mul (Word8 a) (Word8 b) = Word8 $ B.and (a*b) (U.fromInt 0xFF)
+
+instance word8Integral :: Integral Word8 where
+    fromBigInt bi = Word8 $ U.fromNumber <<< BI.toNumber $ bi
+    toBigInt (Word8 a) = BI.fromInt <<< U.toInt $ a
+
+instance word8Bits :: Bits Word8 where
+    and (Word8 a) (Word8 b) = Word8 (B.and a b)
+    or (Word8 a) (Word8 b) = Word8 (B.or a b)
+    xor (Word8 a) (Word8 b) = Word8 (B.xor a b)
+    complement (Word8 a) = Word8 $ B.and (B.complement a) (U.fromInt 0xFF)
+    shl (Word8 a) s = Word8 $ B.and (B.shl a s) (U.fromInt 0xFF)
+    shr (Word8 a) s  = Word8 $ B.shr a s
+    zshr (Word8 a) s = Word8 $ B.zshr a s
+
+
+
+
+
+
+--fromInt :: Int -> Word32
+--fromInt = Word32 <<< ((add (U.fromInt 0)) :: U.UInt -> U.UInt) <<< U.fromInt
+
+--fromUInt :: U.UInt -> Word32
+--fromUInt = Word32
+
+--toInt :: Word32 -> Int
+--toInt (Word32 a) = U.toInt a
+
+--toUInt :: Word32 -> U.UInt
+--toUInt (Word32 a) = a
+
--- test/Main.purs
+++ test/Main.purs
@@ -19,10 +19,6 @@ main = do
     assert' "Word 4 .&. Word 4 == Word 4" $ fromIntegral 4 .&. fromIntegral 4 == fromIntegral 4 :: Word
     assert' "Word 4 .|. Word 2 == Word 6" $ fromIntegral 4 .|. fromIntegral 2 == fromIntegral 6 :: Word
     assert' "Word 4 .^. Word 2 == Word 6" $ fromIntegral 4 .^. fromIntegral 2 == fromIntegral 6 :: Word
---    log $ "top " <> (show (top :: Word))
---    log $ "bottom " <> (show (bottom :: Word))
---    log $ "complenent top " <> (show (complement(top::Word)))
---    log $ "complenent bottom " <> (show (complement(bottom::Word)))
     assert' "complement Word 0x00000000 = complement Word 0xFFFFFFFF" $ complement(fromIntegral 0x00000000) == top :: Word
     assert' "shl (Word 4)  == (Word 8)" $ shl (fromIntegral 4)  (fromInt 1) == (fromIntegral 8) :: Word
     assert' "shr (Word 4)  == (Word 2)" $ shr (fromIntegral 4)  (fromInt 1) == (fromIntegral 2) :: Word
